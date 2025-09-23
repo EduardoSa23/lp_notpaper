7a3ba79ed30a19abb253e1c2a29c556fa0d4afca
Eduardo Sá <111080797+EduardoSa23@users.noreply.github.com>
2025-09-23 14:12:07 -0300

    atualizações

--- js/diferenca.js
+++ js/diferenca.js
@@ -12,28 +12,28 @@ $(document).ready(function () {
       titulo: "Agilidade que Transforma o Serviço",
       subtitulo: "Processos Ágeis",
       descricao:
-        "Reduza drasticamente o tempo de tramitação dos documentos. A inPrint Prime acelera as etapas...",
+        "Reduza drasticamente o tempo de tramitação de documentos e processos. A inPrint Prime acelera as etapas, permite o acompanhamento em tempo real e agiliza a tomada de decisões, resultando em um processo mais rápido e eficaz.",
     },
     {
       img: "./image/implementacao_suporte.png",
       titulo: "Implantação Simples",
       subtitulo: "Parceria Completa",
       descricao:
-        "Nossa equipe garante uma implantação rápida e sem complicações, com treinamento completo...",
+        "Nossa equipe garante uma implantação rápida e sem complicações, com treinamento completo para sua equipe. Conte com suporte técnico especializado e contínuo para que sua prefeitura aproveite ao máximo todos os recursos da inPrint Prime.",
     },
     {
       img: "./image/dados_protegidos.png",
       titulo: "Segurança e Conformidade Garantidas",
       subtitulo: "Dados Protegidos",
       descricao:
-        "Com a inPrint Prime, a segurança dos seus dados é prioridade. Garantimos a integridade...",
+        "Com a inPrint Prime, a segurança dos seus dados é prioridade. Nosso sistema garante a integridade e confidencialidade das informações, com trilhas de auditoria completas e conformidade com a LGPD, assegurando a validade jurídica de cada processo.",
     },
     {
       img: "./image/economia_sustentabilidade.png",
       titulo: "Economia e Sustentabilidade de Mãos Dadas",
       subtitulo: "Digitalização",
       descricao:
-        "A inPrint Prime elimina o uso do papel nos processos, garantindo sustentabilidade e eficiência.",
+        "A inPrint Prime revoluciona a gestão pública, eliminando o uso de papel em todos os processos da sua prefeitura. Do protocolo à assinatura, tudo é digital, organizado e acessível, otimizando o fluxo de trabalho e reduzindo a burocracia.",
     },
   ];
 
